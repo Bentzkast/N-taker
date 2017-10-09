66499674347759a70fdf493c71c47e37552bd4f5
Joseph Alfredo <jalfredopoly@gmail.com>
2017-10-08 20:28:44 -0700

    reconfig db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV === 'production'){
   module.exports = {
     mongoURI:
-    'mongodb://<Bentzkast>:<KW75dw78@>@ds013475.mlab.com:13475/n-taker'};
+    'mongodb://Bentzkast:KW75dw78@@ds013475.mlab.com:13475/n-taker'};
 } else{
   module.exports = {
     mongoURI:
